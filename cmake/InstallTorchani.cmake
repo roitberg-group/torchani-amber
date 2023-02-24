14e7542b13676b4dcb34c13453cb5ff874b7b5db
Ignacio Pickering <ign.pickering@gmail.com>
2023-02-24 15:55:33 -0500

    feature/ignacio/change flag (#4)
    * Add torchani as submodule
    
    * Fix flag

--- cmake/InstallTorchani.cmake
+++ cmake/InstallTorchani.cmake
@@ -1,7 +1,7 @@
 include_guard()
 function(install_torchani)
     set(options "")
-    set(oneValueArgs TORCHVISION_VERSION PYTORCH_VERSION PYTORCH_CUDA PYTORCH_CUDNN PYTORCH_PYTHON)
+    set(oneValueArgs PYTORCH_VERSION PYTORCH_CUDA PYTORCH_CUDNN PYTORCH_PYTHON)
     set(multiValueArgs "")
     cmake_parse_arguments(
         _FN
@@ -19,12 +19,11 @@ function(install_torchani)
     message(STATUS "PyTorch - Installing")
     # select a specific build-string for reproducibility
     set(_BUILD_STRING "=${_FN_PYTORCH_VERSION}=py${_FN_PYTORCH_PYTHON}_cuda${_FN_PYTORCH_CUDA}_cudnn${_FN_PYTORCH_CUDNN}_0")
-    execute_process(COMMAND conda install pytorch=${_FN_PYTORCH_VERSION}=${_BUILD_STRING} torchvision==${_FN_TORCHVISION_VERSION} cudatoolkit=${_FN_PYTORCH_CUDA} -c pytorch -c nvidia -y)
+    execute_process(COMMAND conda install pytorch=${_FN_PYTORCH_VERSION}=${_BUILD_STRING} cudatoolkit=${_FN_PYTORCH_CUDA} -c pytorch -c nvidia -y)
     message(STATUS "PyTorch - Using version ${_FN_PYTORCH_VERSION}")
     message(STATUS "PyTorch - Compatible with CUDA: ${_FN_PYTORCH_CUDA}")
     message(STATUS "PyTorch - For python: ${_FN_PYTORCH_PYTHON}")
     message(STATUS "PyTorch - Compatible with cuDNN: ${_FN_PYTORCH_CUDNN}")
-    message(STATUS "PyTorch - Using Torchvision version ${_FN_TORCHVISION_VERSION}")
 
     # torchani
     message(STATUS "TorchANI - Installing")
