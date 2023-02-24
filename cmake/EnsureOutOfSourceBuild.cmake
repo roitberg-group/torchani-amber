c2924a774b1e83680e3e1300665c4cfa78d392a8
Ignacio Pickering <ign.pickering@gmail.com>
2023-02-24 16:52:56 -0500

    maintain/ignacio/ensure out of source (#5)
    * Ensure out-of-source build
    
    * fix CMakeLists

---
+++ cmake/EnsureOutOfSourceBuild.cmake
@@ -0,0 +1,8 @@
+include_guard()
+include(Msg)
+# Fail if trying an in-source build
+function(ensure_out_of_source_build)
+    if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
+        msg_error("In-source builds are not allowed, you must create a build directory, please remove CMakeFiles/ and CMakeCache.txt.")
+    endif()
+endfunction()
