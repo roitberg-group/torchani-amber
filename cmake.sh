b6b90b56ac994886c55a337acf6edf4d1e3787a3
ipickering <ign.pickering@gmail.com>
2023-03-10 18:14:58 -0500

    Add cmake file

---
+++ cmake.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Usage: ./cmake.sh performs configure && build && install,
+# --conda flag uses conda cuda and conda cudnn
+
+# The directory of this script, taken from:
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+_src_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+_build_dir="$_src_dir/build"
+
+if [ "${1}" = "--conda" ]; then
+    _use_conda=ON
+else
+    _use_conda=OFF
+fi
+
+cmake \
+    --fresh \
+    -S"$_src_dir" \
+    -B"$_build_dir" \
+    -DCONDA_CUDA=$_use_conda \
+    -DCONDA_CUDNN=$_use_conda \
+&& \
+cmake \
+    --build "$_build_dir" \
+&& \
+cmake \
+    --install "$_build_dir"
