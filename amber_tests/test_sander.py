f37467171d47c1d8014075f586e5c6e0262c99eb
ipickering <ign.pickering@gmail.com>
2024-11-07 16:49:09 -0500

    Delete unused files

---
+++ amber_tests/test_sander.py
@@ -0,0 +1,171 @@
+r"""TorchANI-Amber integration tests
+
+If the envvar TORCHANI_AMBER_KEEP_TEST_DIRS=1, then the inputs and outputs
+are not removed after the tests are run.
+"""
+import os
+import tempfile
+import typing as tp
+import itertools
+import unittest
+import subprocess
+from dataclasses import dataclass, asdict
+from pathlib import Path
+
+from parameterized import parameterized
+import jinja2
+
+this_dir = Path(__file__).parent
+Pairlists = tp.Literal["cell_list", "all_pairs", "amber"]
+
+
+@dataclass
+class MlmmConfig:
+    protocol: tp.Literal["me", "mbispol"] = "me"
+
+
+@dataclass
+class CudaConfig:
+    cuaev: bool = True
+
+
+@dataclass
+class RunConfig:
+    cuda: tp.Optional[CudaConfig]
+    mlmm: tp.Optional[MlmmConfig]
+    shake: bool
+    vacuum: bool
+    float64: bool
+    neighborlist: Pairlists
+
+    @property
+    def name(self) -> str:
+        parts = [f"MLMM_{self.mlmm.protocol}"] if self.mlmm else ["FullML"]
+        parts.append("shake" if self.shake else "noshake")
+        parts.append("vac" if self.vacuum else "wat")
+        parts.append("f64" if self.float64 else "f32")
+        if self.cuda:
+            parts.append("cuda")
+            if self.cuda.cuaev:
+                parts.append("cuaev")
+        parts.append(self.neighborlist.replace("_", ""))
+        return "_".join(parts)
+
+    @property
+    def explicit_solvent(self) -> bool:
+        return not self.vacuum
+
+    @property
+    def fullml(self) -> bool:
+        return self.mlmm is None
+
+    @property
+    def cpu(self) -> bool:
+        return self.cuda is None
+
+
+# Generate all configs to be tested
+bools = (True, False)
+mlmm_me = MlmmConfig()
+mlmm_mbispol = MlmmConfig("mbispol")
+cuda_configs = (None, CudaConfig(True), CudaConfig(False))
+mlmm_configs = (None, MlmmConfig("me"), MlmmConfig("mbispol"))
+# TODO test external neighborlist, "amber"
+neighbor_configs: tp.Tuple[Pairlists, ...] = ("cell_list", "all_pairs")
+configs: tp.List[RunConfig] = []
+for tup in itertools.product(
+    cuda_configs, mlmm_configs, bools, bools, bools, neighbor_configs
+):
+    config = RunConfig(*tup)
+
+    # Float 64 is too slow on CPU or with all-pairs
+    if config.float64 and (config.neighborlist == "all_pairs" or config.cpu):
+        continue
+
+    # Mlmm is only tested with all-pairs
+    if config.mlmm:
+        if config.neighborlist != "all_pairs":
+            continue
+
+    # All-Pairs PBC with full-ml is too slow
+    if not config.mlmm:
+        if config.explicit_solvent and config.neighborlist == "all_pairs":
+            continue
+
+    configs.append(config)
+
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(Path(__file__).parent.parent / "templates/"),
+    undefined=jinja2.StrictUndefined,
+    autoescape=jinja2.select_autoescape(),
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
+
+
+def name_func(fn, idx, param) -> str:
+    config, = param.args
+    return f"{fn.__name__}_{param.args[0].name}"
+
+
+class AmberIntegration(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.d: tp.Optional[tempfile.TemporaryDirectory] = None
+
+    def cleanUp(self) -> None:
+        if self.d is not None:
+            self.d.cleanup()
+
+    @parameterized.expand(configs, name_func=name_func)
+    def testSander(self, config: RunConfig) -> None:
+        if os.environ.get("TORCHANI_AMBER_KEEP_TEST_DIRS") == "1":
+            test_dir = Path(__file__).parent / config.name
+            test_dir.mkdir(exist_ok=True)
+        else:
+            self.d = tempfile.TemporaryDirectory()
+            test_dir = Path(self.d.name)
+
+        string = env.get_template("input.mdin.jinja").render(**asdict(config))
+        (test_dir / "input.mdin").write_text(string)
+        self._run_sander(test_dir)
+
+    def _run_sander(self, dir: Path) -> None:
+        # prmtop and inpcrd correspond to a solvated ALA dipeptide
+        this_dir = Path(__file__).parent
+        out = subprocess.run(
+            [
+                "sander",
+                "-i",
+                "input.mdin",
+                "-p",
+                this_dir / "system.prmtop",
+                "-c",
+                this_dir / "system.inpcrd",
+                "-o",
+                "system.mdout",
+                "-r",
+                "system.restart.nc",
+                "-x",
+                "system.traj.nc",
+                "-inf",
+                "system.mdinfo",
+                "-O",  # Overwrite
+            ],
+            cwd=dir,
+            shell=False,
+            capture_output=True,
+            text=True,
+        )
+        if out.returncode != 0:
+            print("Process stdout", out.stdout)
+            print("Process stderr", out.stdout)
+            mdout = (dir / "system.mdout")
+            if mdout.exists():
+                print("Amber mdout", mdout.read_text())
+            raise RuntimeError("Error when calling sander")
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
