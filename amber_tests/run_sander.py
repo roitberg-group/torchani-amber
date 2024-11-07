1106b00278633708a72782db8584be6898c63dbe
ipickering <ign.pickering@gmail.com>
2024-11-07 11:42:19 -0500

    Add test inputs

--- amber_tests/run_sander.py
+++ amber_tests/run_sander.py
@@ -1,30 +1,36 @@
+import sys
 import subprocess
 from pathlib import Path
 
-# prmtop and inpcrd correspond to a solvated ALA dipeptide
-this_dir = Path(__file__).parent
-for d in sorted(this_dir.iterdir()):
-    if not d.is_dir():
-        continue
-    print(f"Running input {d.name}")
-    subprocess.run(
-        [
-            "sander",
-            "-i",
-            "input.md.in",
-            "-p",
-            this_dir / "system.prmtop",
-            "-c",
-            this_dir / "system.inpcrd",
-            "-o",
-            "system.md.out",
-            "-r",
-            "system.restart.nc",
-            "-x",
-            "system.traj.nc",
-            "-O",  # Overwrite
-        ],
-        cwd=d,
-        shell=False,
-        check=True,
-    )
+if __name__ == "__main__":
+    # prmtop and inpcrd correspond to a solvated ALA dipeptide
+    this_dir = Path(__file__).parent
+    for d in sorted(this_dir.iterdir()):
+        if not d.is_dir():
+            continue
+        if "mlmm" in sys.argv and "mlmm" not in d.name:
+            continue
+        print(f"Running input {d.name}")
+        subprocess.run(
+            [
+                "sander",
+                "-i",
+                "input.mdin",
+                "-p",
+                this_dir / "system.prmtop",
+                "-c",
+                this_dir / "system.inpcrd",
+                "-o",
+                "system.mdout",
+                "-r",
+                "system.restart.nc",
+                "-x",
+                "system.traj.nc",
+                "-inf",
+                "system.mdinfo",
+                "-O",  # Overwrite
+            ],
+            cwd=d,
+            shell=False,
+            check=True,
+        )
